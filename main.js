b2bffb20ded933c4f5853fb9d6331961eb06d095
Zulkharnain4510 <71696418+Zulkharnain4510@users.noreply.github.com>
2021-07-31 13:59:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -85,16 +85,15 @@ function draw() {
 
     if(scoreLeftWrist > 0.2)
     {
-    circle(leftWristX, LeftWristY,20);
+    circle(leftWristX, leftWristY,20);
     InNumberleftWristY = Number(leftWristY);
-    remove_decimals = floor(inNumberleftWristY);
-    leftWristY_divide_1000 *2 ;
+    remove_decimals = floor(InNumberleftWristY)
     volume = remove_decimals/500;
     document.getElementById("volume").innerHTML = "Volume = " + volume;
     song.setVolume(volume);
     }
 }
-}
+
 
 function play()
 {
